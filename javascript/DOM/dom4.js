7f49ef404e35b0bc0d489fad4e9e8031f76d19f5
syedansar <syedansar6666@gmail.com>
2021-05-29 14:41:00 +0530

    dom1

---
+++ javascript/DOM/dom4.js
@@ -0,0 +1,21 @@
+console.log('dom4');
+
+// dom paren and siblings traversing
+const article = document.querySelector('article');
+
+// console.log(article.children);
+// Array.from(article);
+// Array.from(article.children)
+
+// Array.from(article.children).forEach((child)=>{
+//     child.classList.add('article-element');
+// });
+
+const title = document.querySelector('h2');
+// console.log(title.parentElement.parentElement.parentElement);
+
+// console.log(title.nextElementSibling.nextElementSibling);
+
+// console.log(title.previousElementSibling);
+
+// console.log(title.nextElementSibling.parentElement.children)
